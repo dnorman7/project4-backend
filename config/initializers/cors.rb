65de25cd9fd9328c6b4f4200756787b67100dc1e
Deanna Norman <deannaleenorman@msn.com>
2021-01-12 06:39:33 -0800

    updates cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost3001'
+    origins 'https://tune-it-up-frontend.herokuapp.com'
 
     resource '*',
       headers: :any,
